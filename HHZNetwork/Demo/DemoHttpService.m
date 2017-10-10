07801285c406ee7485ad338c1790e9868cff9d91
仁和Mac <376811578@qq.com>
2017-10-10 16:58:31 +0900

    no message

--- HHZNetwork/Demo/DemoHttpService.m
+++ HHZNetwork/Demo/DemoHttpService.m
@@ -68,16 +68,17 @@
 
 -(void)handleHttpSuccessErrorInfo:(HHZHttpResponse *)response
 {
-    [self showError:[NSString stringWithFormat:@"%@",[[response.object objectForKey:@"error"] objectForKey:@"msg"]] andErrorCode:[NSString stringWithFormat:@"%@",[[response.object objectForKey:@"error"] objectForKey:@"code"]] andErrorType:response.alertType];
+    [self showErrorTitle:response.object[@"error"][@"errorTitle"] msg:[NSString stringWithFormat:@"%@",response.object[@"error"][@"errorMsg"]] code:[NSString stringWithFormat:@"%@",response.object[@"error"][@"errorCode"]] errorType:response.alertType];
 }
 
 -(void)handleHttpFailInfo:(HHZHttpResponse *)response
 {
-    [self showError:@"网络不给力,请稍候重试!" andErrorCode:@"000000" andErrorType:response.alertType];
+    [self showErrorTitle:nil msg:@"网络不给力,请稍候重试!" code:@"000000" errorType:response.alertType];
 }
 
--(void)showError:(NSString *)errorStr andErrorCode:(NSString *)errorCode andErrorType:(HHZHttpAlertType)type
+-(void)showErrorTitle:(NSString *)title msg:(NSString *)errorMsg code:(NSString *)errorCode errorType:(HHZHttpAlertType)type
 {
+    if (title.length == 0) title = @"网络请求失败";
     switch (type)
     {
         case HHZHttpAlertTypeNone:
@@ -88,6 +89,8 @@
         case HHZHttpAlertTypeNative:
         {
             // 弹Alert框
+            UIAlertView * alert = [[UIAlertView alloc] initWithTitle:title message:errorMsg delegate:nil cancelButtonTitle:nil otherButtonTitles:@"确定", nil];
+            [alert show];
         }
             break;
         case HHZHttpAlertTypeToast:
@@ -100,6 +103,7 @@
             break;
     }
 }
+
 
 -(void)manageBeforeSendRequest:(HHZHttpRequest *)request condition:(HHZHttpRequestCondition *) condition
 {
